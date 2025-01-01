bf09ecb092219d6cb67d9f46e0dcb02eeda880b7
muhammadzaheer <zaheerkhan62145@gmail.com>
2025-01-01 19:49:19 +0500

    ""Webhooks"

---
+++ src/Library/Actions/user.js
@@ -0,0 +1,30 @@
+import User from "../model/user";
+import { ConnectBase } from "../mongoose/ConnectDB";
+
+export const createAndUpdate = async(
+    id,
+    first_name,
+    last_name,
+    image_url,
+    username,
+    email_addresses
+)=>{
+    try {
+        await ConnectBase();
+        const user = User.findOneAndUpdate(
+            {clerkID:id},{
+                $set:{
+                    email:email_addresses[0].email_address,
+                    firstname:first_name,
+                    lastname:last_name,
+                    username:username,
+                    avatar:image_url             
+                }
+            },
+            {new:true,upsert:true}
+        )
+        return user
+    } catch (error) {
+        console.log(`Error in createOrUpdateUSer: ${error}`)    
+    }   
+}
