9c7eb5059bdb951c601ed1874d31d23bd55a68fc
muhammadzaheer <zaheerkhan62145@gmail.com>
2025-01-20 20:13:49 +0500

    "Do it"

--- src/app/api/webhooks/routes.js
+++ src/app/api/webhooks/routes.js
@@ -9,7 +9,7 @@ export async function POST(req) {
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
-      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local Do it.}'
     );
   }
 
