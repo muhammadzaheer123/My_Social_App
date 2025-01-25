2630b30cce1bb9a36e3f7dc2974cc5722c4b8165
muhammadzaheer <zaheerkhan62145@gmail.com>
2025-01-26 01:18:35 +0500

    ""DONE""

--- src/Library/model/user.js
+++ src/Library/model/user.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema(
   {
     clerkId: {
       type: String,
-      required: true,
-      unique: true,
+      required:true,
+      unique:true
     },
     email: {
       type: String,
